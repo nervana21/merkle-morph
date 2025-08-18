b8dc6055f99daf410e9e406a8c4a803007a529e8
nervana21 <205626986+nervana21@users.noreply.github.com>
2025-08-18 18:47:50 -0400

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+#![forbid(unsafe_code)]
+#![warn(missing_docs)]
+#![warn(unreachable_pub)]
+
+//! # Merkle Morph
+//!
+//! A Rust library for unidirectional state channels and
+//! zero-knowledge authentication.
